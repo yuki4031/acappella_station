a4897dfe61f87e678847c722e86b0f89e038accc
kawashimayuuki <yukikawashima1231@gmail.com>
2021-04-11 23:38:07 +0900

    firestoreとの連携。

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -24,14 +24,60 @@ class OurDatabase {
   Future<OurUser> getUserInfo(String uid) async {
     OurUser retVal = OurUser();
 
-    try{
-      DocumentSnapshot _docSnapshot = await _firestore.collection('users').doc(uid).get();
+    try {
+      DocumentSnapshot _docSnapshot =
+          await _firestore.collection('users').doc(uid).get();
       retVal.uid = uid;
       retVal.fullName = _docSnapshot.data()['fullName'];
       retVal.email = _docSnapshot.data()['email'];
       retVal.accountCreated = _docSnapshot.data()['accountCreated'];
+      retVal.groupId = _docSnapshot.data()['groupId'];
+    } catch (e) {
+      print(e);
+    }
 
-    }catch(e){
+    return retVal;
+  }
+
+  Future<String> createGroup(String groupName, String userUid) async {
+    String retVal = 'error';
+    List<String> members = [];
+
+    try {
+      members.add(userUid);
+      DocumentReference _docRef = await _firestore.collection('groups').add({
+        'name': groupName,
+        'leader': userUid,
+        'members': members,
+        'groupCreate': Timestamp.now(),
+      });
+
+      await _firestore.collection('users').doc(userUid).update({
+        'groupId': _docRef.id,
+      });
+
+      retVal = 'success';
+    } catch (e) {
+      print(e);
+    }
+
+    return retVal;
+  }
+
+  Future<String> joinGroup(String groupId, String userUid) async {
+    String retVal = 'error';
+    List<String> members = [];
+
+    try {
+      members.add(userUid);
+      await _firestore.collection('groups').doc(groupId).update({
+        'members': FieldValue.arrayUnion(members),
+      });
+      await _firestore.collection('users').doc(userUid).update({
+        'groupId': groupId,
+      });
+      retVal = 'success';
+    } catch (e) {
       print(e);
     }
 
